4aec265c24a0fe56260a7f2d96b53d15a86b57cd
Harshad.N <harshad.n4321@gmail.com>
2023-02-09 20:41:31 +0530

    added folder

---
+++ products/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World! this hopping' });
+});
+
+
+app.listen(port, () => console.log(`server is running on port http://localhost:${port}`))
+
