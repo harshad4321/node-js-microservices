2b6fdd52771010eae7a75c5711d2ae0a04393673
Harshad.N <harshad.n4321@gmail.com>
2023-02-10 11:27:26 +0530

    src is added

--- gateway/index.js
+++ gateway/index.js
@@ -4,15 +4,15 @@ const proxy = require('express-http-proxy')
 
 
 const app = express()
-
+app.use(cors())
 app.use(express.json());
 
 const port = process.env.PORT || 1000
 
-app.use('/', proxy(`http://localhost:5000`)) //products -> root endpoint
 app.use('/customer', proxy(`http://localhost:3000`))
 app.use('/shopping', proxy(`http://localhost:8000`))
+app.use('/', proxy(`http://localhost:5000`)) //products -> root endpoint
 
 
-app.listen(port, () => console.log(`server is running on port http://localhost:${port}`))
+app.listen(port, () => console.log(`server Gateyay is running on port http://localhost:${port}`))
 
